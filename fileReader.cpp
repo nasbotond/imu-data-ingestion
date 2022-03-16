919cccfb619d90d63f466047285cb3ed4d27f2de
Botond A. Nas <botond@Botonds-MacBook-Pro.local>
2022-03-16 18:57:11 +0100

    combined fileReader and videoReader and added test to video

--- fileReader.cpp
+++ fileReader.cpp
@@ -1,4 +1,5 @@
 #include "opencv2/opencv.hpp"
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <fstream>
 
@@ -39,7 +40,7 @@ int main()
     numFrames = cap.get(CAP_PROP_FRAME_COUNT);
 
     // system("pause");
-    cap.release();
+    // cap.release();
 
     // There are 4963 files in total...
     // Create a text string, which is used to output the text file
@@ -51,7 +52,7 @@ int main()
     vector<float> altitude; // m
     vector<float> speed; // km/h
 
-    for(int i = 1; i <= 100; i++)
+    for(int i = 1; i <= 1000; i++) // this should be 1 -> numFrames
     {
         string path = "../../20220127_IMU_data/alagut_pol/test_fn" + to_string(i) + ".pol";
         // Read from the text file
@@ -89,11 +90,61 @@ int main()
     }
 
     // print all elements
-    cout << "vector elements are: ";
-    for (int i = 0; i < speed.size(); ++i) {
-        cout << speed[i] << ' ';
-    }
-    cout << endl;
+    // cout << "vector elements are: ";
+    // for (int i = 0; i < speed.size(); ++i) {
+    //     cout << speed[i] << ' ';
+    // }
+    // cout << endl;
     // cout << "Total Number of frames are:" << numFrames << endl;
+
+    // Create a VideoCapture object and open the input file
+    // If the input is the web camera, pass 0 instead of the video file name
+    // VideoCapture cap("../../20220127_IMU_data/Alagut_3cam_4FPS_GPS_IMU_preview.mp4");
+
+    // Check if camera opened successfully
+    if(!cap.isOpened())
+    {
+        cout << "Error opening video stream or file" << endl;
+        return -1;
+    }
+
+    int i = 0;
+    while(1)
+    {
+        Mat frame;
+        // Capture frame-by-frame
+        cap >> frame;
+
+        // If the frame is empty, break immediately
+        if(frame.empty())
+            break;
+
+        putText(frame, 
+            "Speed: " + to_string(speed[i]),
+            cv::Point(15,15), // Coordinates (Bottom-left corner of the text string in the image)
+            cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+            1.0, // Scale. 2.0 = 2x bigger
+            cv::Scalar(255,255,255), // BGR Color
+            1, // Line Thickness (Optional)
+            cv:: LINE_AA); // Anti-alias (Optional, see version note)
+
+        // Display the resulting frame
+        imshow( "Frame", frame );
+
+        // Press  ESC on keyboard to exit
+        char c = (char)waitKey(25);
+        if(c == 27)
+            break;
+
+        i++;
+    }
+
+    // When everything done, release the video capture object
+    cap.release();
+
+    // Closes all the frames
+    destroyAllWindows();
+
+    return 0;
 }
 
